a4f45a0f01429788c8f75a606c7f3b5cec121c1a
navindunimsara2001 <navidunimsara@gamil.com>
2026-03-21 19:41:31 +0530

    docker setup

--- services/book-service/src/app.controller.ts
+++ services/book-service/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { version } from '../package.json';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/api/health')
+  getHealth(): any {
+    return { healthy: true, version: version };
   }
 }
