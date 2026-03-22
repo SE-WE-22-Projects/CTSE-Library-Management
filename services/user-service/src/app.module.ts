2a1be8122f6510eec8dc0cb98e1bfbc711ff2573
yehan2002 <yehanjaya2002@gmail.com>
2026-03-22 13:31:16 +0530

    Remove keycloak.

--- services/user-service/src/app.module.ts
+++ services/user-service/src/app.module.ts
@@ -3,17 +3,17 @@ import { HealthModule } from './modules/health/health.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { databaseConfig } from './config/database.config';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-
     MongooseModule.forRootAsync({
       useFactory: databaseConfig,
     }),
-
+    UserModule,
     HealthModule,
   ],
 })
--- services/user-service/src/modules/user/user.controller.ts
+++ services/user-service/src/modules/user/user.controller.ts
@@ -1,4 +1,3 @@
-import { Resource, Roles, Scopes, Public } from 'nest-keycloak-connect';
 import {
   Controller,
   Get,
@@ -12,37 +11,30 @@ import { User } from './user';
 import { UserService } from './user.service';
 
 @Controller('api/user')
-@Resource(User.name)
 export class UserController {
   constructor(private service: UserService) {}
 
   @Get()
-  @Public()
   findAll() {
     return this.service.findAll();
   }
 
   @Get(':id')
-  @Scopes('View')
   findByCode(@Param('id') id: string) {
     return this.service.findById(id);
   }
 
   @Post()
-  @Scopes('Create')
   create(@Body() user: User) {
     return this.service.create(user);
   }
 
   @Delete(':id')
-  @Scopes('Delete')
-  @Roles({ roles: ['admin'] })
   deleteById(@Param('id') id: string) {
     return this.service.deleteById(id);
   }
 
   @Put(':id')
-  @Scopes('Edit')
   update(@Param('id') id: string, @Body() user: User) {
     return this.service.update(id, user);
   }
