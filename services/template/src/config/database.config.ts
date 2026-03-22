15622e9501feff5ac9a1a7d08d8bc3f54b93f035
Kasun Hettiarachchi <kasunh1411@gmail.com>
2026-03-22 19:21:43 +0530

    Template service removed

--- services/template/src/config/database.config.ts
+++
@@ -1,7 +0,0 @@
-import { MongooseModuleFactoryOptions } from '@nestjs/mongoose';
-import { name } from '../../package.json';
-
-export const databaseConfig = (): MongooseModuleFactoryOptions => ({
-  uri: process.env['MONGO_URI']!,
-  dbName: name,
-});
