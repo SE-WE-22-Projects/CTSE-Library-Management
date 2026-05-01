13ad9c73ddfbcd45cf838ead05934930933ecc1f
navindunimsara2001 <navidunimsara@gmail.com>
2026-05-01 14:36:03 +0530

    config func

--- services/book-service/src/config/database.config.ts
+++ services/book-service/src/config/database.config.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 
+// db config func
 export const databaseConfig = async (configService: ConfigService) => ({
     uri: configService.get<string>('MONGO_URI'),
 });
