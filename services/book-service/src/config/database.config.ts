a347bbc22c9b91824c8a9bb9b79efb176baeec79
navindunimsara2001 <navidunimsara@gamil.com>
2026-05-01 14:31:07 +0530

    sr 1

--- services/book-service/src/config/database.config.ts
+++ services/book-service/src/config/database.config.ts
@@ -1,7 +1,6 @@
 import { MongooseModuleFactoryOptions } from '@nestjs/mongoose';
 import { name } from '../../package.json';
 
-
 export const databaseConfig = (): MongooseModuleFactoryOptions => ({
   uri: process.env['MONGO_URI']!,
   dbName: name,
